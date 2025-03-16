1e3fcd1336d70e4c92443d3a342c3c9752374950
tqnie <tigertheblack2000@gmail.com>
2025-03-16 06:42:33 +0200

    sa

--- app/Livewire/ProductUsers.php
+++ app/Livewire/ProductUsers.php
@@ -17,21 +17,24 @@ class ProductUsers extends Component
     public $slug;
 
     public ?Product $product;
-    public $users = [];
+    
     public function mount()
     {
 
         if ($this->slug) {
             $this->product = Product::where('slug', $this->slug)->first();
-              $this->users = User::whereHas('offers', function ($query) {
-                return $query->where('product_id', $this->product->id);
-            })->withCount(['offers as seller' => fn($q) => $q->where('product_id', $this->product->id)->where('type', 'seller'), 'offers as buyer' => fn($q) => $q->where('product_id', $this->product->id)->where('type', 'buyer')])->get();
-        }
+            }
         
     }
 
+    public function users(){
+        return  User::whereHas('offers', function ($query) {
+            return $query->where('product_id', $this->product->id);
+        })->withCount(['offers as seller' => fn($q) => $q->where('product_id', $this->product->id)->where('type', 'seller'), 'offers as buyer' => fn($q) => $q->where('product_id', $this->product->id)->where('type', 'buyer')])->get();
+    
+    }
     public function render()
     {
-        return view('livewire.product-users',);
+        return view('livewire.product-users',['users'=>$this->users()]);
     }
 }
