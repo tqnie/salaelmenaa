327a79eba82af2b0c5f9799ba10179204f557601
tqnie <tigertheblack2000@gmail.com>
2025-04-03 05:21:46 +0200

    e

--- resources/views/livewire/product-users.blade.php
+++ resources/views/livewire/product-users.blade.php
@@ -53,7 +53,7 @@
                                 <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>{{$user->seller+$user->buyer}} </li>
                                 <li class="back-book"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-book"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg> {{$product->title}}</li>
                             </ul>
-                            <h4 class="case-title"> <a href="{{route('offers.user',[$product->slug,$user->id])}}">{{$user->bio}}</a></h4>
+                            <h4 class="case-title"> <a href="{{route('offers.user',[$product->slug,$user->productUser->type,$user->id])}}">{{$user->bio}}</a></h4>
                             <div class="back__user">
                                 <img src="{{$user->avatar}}" alt="user">{{$user->name}}
                             </div>
