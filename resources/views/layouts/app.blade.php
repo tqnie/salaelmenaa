839d00ec04144820a640edf724662c53f31c56a2
tqnie <tigertheblack2000@gmail.com>
2025-04-08 02:40:46 +0200

    ss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,13 +66,17 @@
                             <div class="col-lg-3 md-mb-30">
                                 <div class="footer-widget footer-widget-1">
                                     <div class="footer-logo white">
-                                        <a href="index.html" class="logo-text"> <img src="assets/images/logo-light.png" alt="logo"></a>
+                                        <a href="index.html" class="logo-text"> <img src="{{asset('assets/images/logo-light.png')}}" alt="{{setting('site_name')}}"></a>
                                     </div>
                                     <h5 class="footer-subtitle">نبذة عن الموقع هنا </h5>
                                     <ul class="footer-address">
-                                         <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#f84e77" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg><a href="tel:+(462)42576332">+96653 </a></li>
-                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#f84e77" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><a href="mailto:info@yourdmain.com">info@gmail.com </a></li>
-                                    </ul>                                
+                                         <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#f84e77" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg><a href="tel:{{setting('mobile')}}">{{setting('mobile')}} </a></li>
+                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#f84e77" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><a href="mailto:{{setting('email')}}">{{setting('email')}}</a></li>
+                                    </ul> 
+                                    <h6 class="back-follow-us">تابعنا علي </h6>
+                                    <ul class="social-links">
+                                         <li><a href="{{setting('twitter')}}"><span aria-hidden="true" class="social_twitter"></span></a></li>
+                                     </ul>                                 
                                 </div>
                             </div>
                             <div class="col-lg-3 md-mb-30">
