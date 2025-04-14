820e326ce02a943a690b0852aa0f0a25ff586555
tqnie <tigertheblack2000@gmail.com>
2025-04-14 18:10:42 +0200

    dd

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -39,7 +39,7 @@
                  <div class="back-sidebar">
                     <div class="back__title__section text-center m-5">
                         <div class="col-md-6 back-search">
-                            <form wire:submit="products">
+                            <form wire:submit="products" class="mb-3">
                                 <input type="text" wire:model="query" name="input" placeholder="بحث...">
                                 <button> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg> </button>
                             </form>
