35bc4566999d33f5cafdf8769e2f7eb74e597588
tqnie <tigertheblack2000@gmail.com>
2025-04-14 18:12:32 +0200

    qwe

--- resources/views/livewire/product-users.blade.php
+++ resources/views/livewire/product-users.blade.php
@@ -7,7 +7,7 @@
                 <div class="back__course__area   back-sidebar">          
                     <div class="row align-items-center back-vertical-middle shorting__course mb-50 ">
                         <div class="col-md-6 back-search">
-                            <form action="">
+                            <form action=""  class="mb-3">
                                 <input type="text" name="input" placeholder="بحث...">
                                 <button> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg> </button>
                             </form>
